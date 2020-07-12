f5d079d9df11fcee89fce328376399d86705c4fa
qtdevops <qtdevops@gmail.com>
2020-07-12 16:03:20 +0530

    Added fix

--- app4/app.py
+++ app4/app.py
@@ -8,4 +8,4 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Sample Application for microservices</h1><p>This site is a prototype API for app4.</p>"
 
-app.run()
+app.run(port=5000, debug=True, host="0.0.0.0")
